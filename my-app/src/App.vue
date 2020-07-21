3c78b7cf19e3f1dc17b39729ec8d31ab81a80834
Kevin <r_kevin@hotmail.fr>
2020-07-21 16:01:45 +0200

    install

---
+++ my-app/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app" class="container">
+    <br />
+    <h1>Intégration bootstrap</h1>
+    <br />
+    <p>
+      <b-btn v-b-toggle.collapse1 variant="warning">Toggle Collapse</b-btn>
+    </p>
+    <b-collapse id="collapse1">
+      <b-card>
+        Collapse contents Here
+        <b-btn v-b-toggle.collapse2 size="sm">Toggle Inner Collapse</b-btn>
+        <b-collapse id=collapse2 class="mt-2">
+          <b-card>Hello!</b-card>
+        </b-collapse>
+      </b-card>
+    </b-collapse>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+  }
+</script>
+
+<style lang="scss">
+  @import "./assets/custom.scss";
+  @import "../node_modules/bootstrap/scss/bootstrap.scss";
+</style>
