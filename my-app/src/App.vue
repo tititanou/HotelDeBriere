8154145f66d7f870e4ed3ac38408a23ab6682d21
Kevin <r_kevin@hotmail.fr>
2020-07-22 14:53:26 +0200

    HomePage started

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -1,27 +1,31 @@
 <template>
-  <div id="app" class="container">
-    <br />
-    <h1>Intégration bootstrap</h1>
-    <br />
-    <p>
-      <b-btn v-b-toggle.collapse1 variant="warning">Toggle Collapse</b-btn>
-    </p>
-    <b-collapse id="collapse1">
-      <b-card>
-        Collapse contents Here
-        <b-btn v-b-toggle.collapse2 size="sm">Toggle Inner Collapse</b-btn>
-        <b-collapse id=collapse2 class="mt-2">
-          <b-card>Hello!</b-card>
-        </b-collapse>
-      </b-card>
-    </b-collapse>
+  
+  <div id="app">
+    <header>
+      <Header />
+    </header>
+
+    <body>
+      <Home />
+    </body>
+      
+    
+    
   </div>
 </template>
 
 <script>
+
+import Header from "@/components/Header.vue";
+import Home from "@/components/Home.vue";
+
   export default {
-    name: 'app',
-  }
+    components: {
+      Header,
+      Home,
+    }
+  };
+
 </script>
 
 <style lang="scss">
---
+++ my-app/src/components/Home.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-3">
+                <homePresentation />            
+            </div>
+            <div class="col-6 mt-5">
+                    <homeArticle />
+                    <homeArticle />      
+                    <homeArticle />
+            </div>
+            <div class="col-3">
+                <div class="row">
+                    <p>blablabla</p>
+                </div>
+                <div class="row">
+                    <p>blablabla</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import homePresentation from "@/components/homePresentation.vue";
+import homeArticle from "@/components/homeArticle.vue";
+//import homeProvisoire from "@/components/homeProvisoire.vue";
+
+
+export default {
+    components: {
+      homePresentation,
+      homeArticle,
+      //homeProvisoire
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
