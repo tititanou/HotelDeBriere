66abeecc4c6969474a4a7db3bcae44775926714e
Kevin <r_kevin@hotmail.fr>
2020-07-22 15:17:51 +0200

    header

--- my-app/src/components/Header.vue
+++ my-app/src/components/Header.vue
@@ -1,14 +1,29 @@
 <template>
   <div>
   <b-navbar toggleable="lg" type="dark" variant="info">
-    <b-navbar-brand href="#">NavBar</b-navbar-brand>
+    <b-navbar-brand href="#">HOTEL DE BRIERE</b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
-        <b-nav-item href="#">Link</b-nav-item>
-        <b-nav-item href="#">Disabled</b-nav-item>
+        <b-nav-item-dropdown text="L'association" right>
+          <b-dropdown-item href="#">Présentation</b-dropdown-item>
+          <b-dropdown-item href="#">Son histoire</b-dropdown-item>
+          <b-dropdown-item href="#">Nous joindre</b-dropdown-item>
+          <b-dropdown-item href="#">Partenaires</b-dropdown-item>
+        </b-nav-item-dropdown>
+        <b-nav-item-dropdown text="Le projet" right>
+          <b-dropdown-item href="#">L'emergence</b-dropdown-item>
+          <b-dropdown-item href="#">La convention</b-dropdown-item>
+        </b-nav-item-dropdown>
+        <b-nav-item href="#">La phase d'amorçage</b-nav-item>
+        <b-nav-item href="#">Le chantier</b-nav-item>
+        <b-nav-item href="#">Evenements</b-nav-item>
+        <b-nav-item href="#">On en parle</b-nav-item>
+        <b-nav-item href="#">Ressources</b-nav-item>
+        
+
       </b-navbar-nav>
 
       <!-- Right aligned nav items -->
@@ -18,12 +33,6 @@
           <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
         </b-nav-form>
 
-        <b-nav-item-dropdown text="Lang" right>
-          <b-dropdown-item href="#">EN</b-dropdown-item>
-          <b-dropdown-item href="#">ES</b-dropdown-item>
-          <b-dropdown-item href="#">RU</b-dropdown-item>
-          <b-dropdown-item href="#">FA</b-dropdown-item>
-        </b-nav-item-dropdown>
 
         <b-nav-item-dropdown right>
           <!-- Using 'button-content' slot -->
