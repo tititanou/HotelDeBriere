97c5a6a930a6e9cefd22b608ac199d1dde706b7f
Kevin <r_kevin@hotmail.fr>
2020-08-12 15:02:17 +0200

    tags et sketchfab ( mtest modf css)

--- my-app/src/components/newArticle.vue
+++ my-app/src/components/newArticle.vue
@@ -4,48 +4,47 @@
         <h1>Création de l'article:</h1>
         <div>
             
-            <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+            <b-form v-on:keypress.enter.prevent @submit="onSubmit" @reset="onReset" v-if="show">
                 
-                <b-form-group id="input-group-1" label="Titre:" label-for="input-1">
-                    <b-form-input id="input-1" v-model="form.title" type="text" required placeholder="Titre"></b-form-input>
+                <b-form-group label="Titre:">
+                    <b-form-input v-model="form.title" type="text" required placeholder="Titre"></b-form-input>
                 </b-form-group>
 
-                <b-form-group id="input-group-2" label="Sous-titre:"  label-for="input-2" description="Facultatif">
-                    <b-form-input id="input-2" v-model="form.subtitle" placeholder="Sous-titre"></b-form-input>
+                <b-form-group label="Sous-titre:" description="Facultatif">
+                    <b-form-input v-model="form.subtitle" placeholder="Sous-titre"></b-form-input>
                 </b-form-group>
 
-                <b-form-group id="input-group-4" label="Résumé:" label-for="input-4">
-                    <b-form-textarea id="textarea" v-model="form.abstract" placeholder="Résumé" rows="3" max-rows="6"></b-form-textarea>
+                <b-form-group label="Résumé:">
+                    <b-form-textarea v-model="form.abstract" placeholder="Résumé" rows="3" max-rows="6"></b-form-textarea>
                 </b-form-group>
 
-                <b-form-group id="input-group-3" label="Contenu de l'article:" label-for="input-3" >
+                <b-form-group label="Contenu de l'article:">
                     <b-form>
-                        <vue-editor label="Contenu de l'article:" id="input-3" required v-model="form.content"></vue-editor>
+                        <vue-editor label="Contenu de l'article:" required v-model="form.content"></vue-editor>
                     </b-form>
                 </b-form-group>
 
-                <b-form-group 
-                    id="input-group-4" 
-                    label="Date de mise en ligne:" 
-                    label-for="input-4"
-                >
-                    <b-form-datepicker id="input-4" v-model="form.releaseDate" required class="mb-2"></b-form-datepicker>
+                <b-form-group label="Date de mise en ligne:">
+                    <b-form-datepicker v-model="form.releaseDate" required class="mb-2"></b-form-datepicker>
                 </b-form-group>
 
-                <b-form-group 
-                    id="input-group-4" 
-                    label="Categorie:" 
-                    label-for="input-4"
-                >
-                    <b-form-input list="input-list" id="input-with-list" required v-model="form.category"></b-form-input>
-                    <b-form-datalist id="input-list" :options="options"></b-form-datalist>
+                <b-form-group label="Catégorie:">
+                    <b-form-input placeholder="Ajouter une catégorie en appuyant sur Entrée" list="input-list" v-model="tag" @keyup="handleTyping"></b-form-input>
+                    <b-form-datalist id="input-list" :options="propTags"></b-form-datalist>
+                    <div class="tags mt-2">
+                        <div v-for="(_tag, index) in form.tags" :key="index" class="tag" @click="removeTag(index)">{{ _tag }}</div>
+                    </div>                
+                </b-form-group>
+                <!--<b-form-input type="text" placeholder="Add tags separated by ," v-model="tag" @keyup="handleTyping"></b-form-input>
+                <label>Tags</label>
+                <div class="tags">
+                    <div v-for="(_tag, index) in tags" :key="index" class="tag" @click="removeTag(index)">{{ _tag }}</div>
+                </div>-->
+                <b-form-group label="Auteur de l'article:">
+                    <b-form-input v-model="form.autor" type="text" required placeholder="Auteur"></b-form-input>
                 </b-form-group>
 
-                <b-form-group id="input-group-4" label="Auteur de l'article:" label-for="input-4">
-                    <b-form-input id="input-1" v-model="form.autor" type="text" required placeholder="Auteur"></b-form-input>
-                </b-form-group>
-
-                <b-form-group id="input-group-4" label="Photo de présentation:" label-for="input-4">
+                <b-form-group label="Photo de présentation:">
                    <b-form v-model="picture">
                        <div >
                             <input type="file" @change="previewImage" accept="image/*" >
@@ -62,8 +61,9 @@
                    </b-form>
                 </b-form-group>
 
-                <b-form-group label="Video de présentation" description="facultatif">
-                    <b-form-input id="input-1" v-model="form.video" type="text" placeholder="Lien vers une vidéo"></b-form-input>
+                <b-form-group label="Média de présentation:" description="facultatif">
+                    <b-form-input v-model="form.media" type="text" placeholder="Lien vers un média"></b-form-input>
+                    <b-checkbox v-model="form.is3DReal">Cocher si c'est une réalisation 3D SketchFab</b-checkbox>
                 </b-form-group>
 
         <div class="mt-5">
@@ -90,11 +90,25 @@
             </div>
             <div>
                 <h2>Page Complète:</h2> 
-                <div v-if="form.video"
+                <b-card>
+                <div v-if="form.media && form.is3DReal == false"
                 class="mb-4 embed-responsive embed-responsive-16by9"
-
                 >
-                <iframe class="col-12 embed-responsive-item" :src="form.video"  frameborder="0"></iframe>
+                <iframe class="col-12 embed-responsive-item" :src="form.media"  frameborder="0"></iframe>
+                </div>
+                <div v-if="form.media && form.is3DReal">
+                    <div class="sketchfab-embed-wrapper mb-4 embed-responsive embed-responsive-16by9">
+                        <iframe 
+                            class="col-12 embed-responsive-item"
+                            title="A 3D model"
+                            :src="form.media" 
+                            frameborder="0" 
+                            allow="autoplay; fullscreen; vr" 
+                            mozallowfullscreen="true" 
+                            webkitallowfullscreen="true"
+                            >
+                        </iframe>
+                    </div>
                 </div>
                 <div v-else>
                     <img :src="form.picture" class="col-12">
@@ -103,13 +117,16 @@
                     <h1>{{form.title}}</h1>
                     <h3>{{form.subtitle}}</h3>
                 <div class="row">
-                    <div class="col">{{form.releaseDate}}</div> 
-                    <div class="col">{{form.autor}}</div>  
-                    <div class="col">{{form.category}}</div>
+                    <div class="col">Mis en ligne le: {{form.releaseDate}}</div> 
+                    <div class="col">Ecrit part: {{form.autor}}</div>  
+                    <div class="col">Catégories: 
+                        <div class="raw" v-for="(_tag, index) in form.tags" :key="index">{{_tag}}</div>
+                    </div>
                 </div>
                 
                 <div v-html="form.content"></div>
                 </div>
+                </b-card>
 
         </div>
     </div>
@@ -120,7 +137,7 @@
 
             <!--<b-card class="mt-3" header="Form Data Result">
                 <pre class="m-0">{{ form }}</pre>
-                <pre class="m-0">{{ picture }}</pre>
+                
             </b-card>-->
         </div>
     </div>
@@ -134,8 +151,8 @@ import { VueEditor } from "vue2-editor";
 
 export default {
   components: {
-    VueEditor,
-  },
+    VueEditor, 
+    },
   
   data() {
     return {
@@ -149,64 +166,91 @@ export default {
             content: '',
             releaseDate: '',
             autor: '',
-            category: '',
+            tags: [],
             picture: '',
-            video: '',
+            media: '',
+            is3DReal: false,
         },
-        options: ['Apple', 'Banana', 'Grape', 'Kiwi', 'Orange'],
+        tag: '',
+        propTags: [],
         show: true
       }
     },
+    created(){
+        this.display(this.propTags)
+    },
     methods: {
-      onSubmit(evt) {
-        evt.preventDefault();
-        //alert(JSON.stringify(this.form))
-        if(this.form.abstract.length == 0 || this.form.abstract.length > 280){
-            alert("Vous devez écrire un résumé")
+        display(list){        
+         let ref = firebase.database().ref('tags');
+            ref.once('value', function(snapshot) {
+                snapshot.forEach(function(childSnapshot) {
+                    var childKey = childSnapshot.key;
+                    console.log(childKey);
+                    list.push(childKey);
+                });
+            });
+            console.log(list.length);
+       
+     },
+    onSubmit(evt) {
+    evt.preventDefault();
+    //alert(JSON.stringify(this.form))
+    if(this.form.abstract.length == 0 || this.form.abstract.length > 280){
+        alert("Vous devez écrire un résumé de 280 caractères maximum.")
+    }
+    else if (this.form.content.length == 0){
+        alert("Vous devez écrire un article.")
+    }
+    else if (this.form.releaseDate.length == 0){
+        alert("Vous devez entrer une date.")
+    }
+    else if (this.form.picture.length == 0){
+        alert("Vous devez entrer une image.")
+    }
+    else if (this.form.tags.length == 0){
+        alert("Vous devez entrer au moins une catégorie.")
+    }
+    else{
+        for(let i=0; i < this.form.tags.length; i++){
+            firebase.database().ref('tags/'+ this.form.tags[i]).set({
+                tag: this.form.tags[i]
+            })
         }
-        else if (this.form.content.length == 0){
-            alert("Vous devez écrire un article de 280 caractères maximum.")
-        }
-        else if (this.form.releaseDate.length == 0){
-            alert("Vous devez entrer une date.")
-        }
-        else if (this.form.picture.length == 0){
-            alert("Vous devez entrer une image.")
-        }
-        else{
-            console.log(this.form.content.length)
-            firebase.database().ref('articles/').push({
-                title: this.form.title,
-                subtitle: this.form.subtitle,
-                abstract: this.form.abstract,
-                content: this.form.content,
-                releaseDate: this.form.releaseDate,
-                autor: this.form.autor,
-                category: this.form.category,
-                picture: this.form.picture,
-                video: this.form.video,
-            }).then(alert("Votre article a été créé avec succès."));
-        }
-      },
-      onReset(evt) {
-        evt.preventDefault()
-        // Reset our form values
-        this.form.title = ''
-        this.form.subtitle = ''
-        this.form.abstract = ''
-        this.form.content = ''
-        this.form.releaseDate = ''
-        this.form.autor = ''
-        this.form.category = ''
-        this.form.picture = ''
-        this.form.video = ''
-        // Trick to reset/clear native browser form validation state
-        this.show = false
-        this.$nextTick(() => {
-          this.show = true
-        })
-      },
-      previewImage(event) {
+        console.log(this.form.content.length)
+        firebase.database().ref('articles/').push({
+            title: this.form.title,
+            subtitle: this.form.subtitle,
+            abstract: this.form.abstract,
+            content: this.form.content,
+            releaseDate: this.form.releaseDate,
+            autor: this.form.autor,
+            tags: this.form.tags,
+            picture: this.form.picture,
+            media: this.form.media,
+            is3DReal: this.form.is3DReal,
+        }).then(alert("Votre article a été créé avec succès."));
+    }
+    },
+    onReset(evt) {
+    evt.preventDefault()
+    // Reset our form values
+    this.form.title = ''
+    this.form.subtitle = ''
+    this.form.abstract = ''
+    this.form.content = ''
+    this.form.releaseDate = ''
+    this.form.autor = ''
+    this.form.tags = []
+    this.form.picture = ''
+    this.form.media = ''
+    this.form.is3DReal = false
+    // Trick to reset/clear native browser form validation state
+    this.show = false
+    this.$nextTick(() => {
+        this.show = true
+    })
+    },
+    previewImage(event) {
       this.uploadValue=0;
       this.picture=null;
       this.imageData = event.target.files[0];
@@ -223,6 +267,29 @@ export default {
           this.form.picture =url;
         });
       });
+    },
+    addTag(tag) {
+        if(tag!= ''){
+            if(this.form.tags.length < 3){
+                let newTag = tag.toLowerCase();
+                this.form.tags.push(newTag);
+            }
+        }
+    },
+    removeTag(index) {
+        this.form.tags.splice(index, 1);
+    },
+    tagExists(tag) {
+        return this.form.tags.indexOf(tag) !== -1;
+    },
+    handleTyping(e) {
+        if ( e.keyCode === 13 ) {
+            let tag = this.tag.replace(/,/g, '');
+            if ( !this.tagExists(tag) ) {
+                this.addTag(tag);
+                this.tag = '';
+            }
+        }
     },
 
     createArticle(){
