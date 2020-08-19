48c16a047ec45013ab42d574fc58325d9364f248
tititanou <tania.rojas@edu.itescia.fr>
2020-08-19 12:48:55 +0200

    vérif admin

--- my-app/src/components/AdminManage.vue
+++ my-app/src/components/AdminManage.vue
@@ -1,6 +1,7 @@
 <template>
     <b-container>
-        <b-card title="Portail administrateur" class="mb-4 ">
+        <b-card title="Portail administrateur" class="mb-4 " v-if="isConnected && isAdmin">
+            <p>Bienvenue {{currentU.firstname}}!</p>
             <b-card-group deck>
                 <b-col>
                     <b-row class="my-1">
@@ -45,5 +46,62 @@
                 </b-col>
             </b-card-group>
         </b-card>
+        <b-card title="Portail administrateur" class="mb-4 " v-else-if="isConnected && !isAdmin">
+            <p>Accès refusé! Vous devez être administrateur pour accéder au contenu de cette page.</p>
+            <b-button variant="primary" href="/">Retour à l'acceuil</b-button>
+        </b-card>
+        <b-card title="Portail administrateur" class="mb-4 " v-else>
+            <p>Accès refusé! Vous devez vous connecter.</p>
+            <b-button variant="primary" href="/">Retour à l'acceuil</b-button>
+            <b-button variant="primary" href="inscriptionConnexion">Se connecter</b-button>
+        </b-card>
     </b-container>
 </template>
+
+<script>
+import firebase from 'firebase'
+export default {
+    data(){
+        return {
+            isConnected: false,
+            isAdmin: false,
+            user: '',
+            currentU:{
+                firstname:'',
+                name:'',
+                admin:false
+            }
+        }
+    },
+    created(){
+        this.user = firebase.auth().currentUser;
+        if (this.user != null){
+            this.isConnected = true;
+        }
+    },
+    mounted(){
+        let viewUser = this;
+        viewUser.user = firebase.auth().currentUser;
+        const usersRef = firebase.database().ref("users");
+        usersRef.on("value", snapshot => {
+            let data = snapshot.val();
+            let users = [];
+            Object.keys(data).forEach(key =>{
+                if (key == viewUser.user.uid){
+                    users.push({
+                        firstname: data[key].firstname,
+                        name: data[key].name,
+                        admin: data[key].admin
+                    });
+                }
+            });
+            if(users.length > 0) {
+                viewUser.currentU.firstname = users[0].firstname;
+                viewUser.currentU.name = users[0].name;
+                viewUser.currentU.admin = users[0].admin;
+                viewUser.isAdmin = viewUser.currentU.admin;
+            }
+        });
+    }
+}
+</script>
